f83d15d2756f0c213210fd3929e8692595aa3db6
Eatmeta <metagroul@gmail.com>
2023-02-06 17:07:44 +0300

    Add default clients

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,4 +1,7 @@
 using System.Reflection;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.Models;
 using IdentityServer.Data;
 using IdentityServer.Factories;
 using IdentityServer.Models;
@@ -32,6 +35,103 @@ builder.Services.AddIdentityServer()
     });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+
+    await scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.MigrateAsync();
+    await scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.MigrateAsync();
+    await scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.MigrateAsync();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    if (await userManager.FindByNameAsync("eat.meta") == null)
+    {
+        await userManager.CreateAsync(
+            new ApplicationUser
+            {
+                UserName = "eat.meta",
+                Email = "eat.meta@example.com",
+                GivenName = "Eat",
+                FamilyName = "Meta"
+            }, "Pa55w0rd!");
+    }
+
+    var configurationDbContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+
+    if (!await configurationDbContext.ApiResources.AnyAsync())
+    {
+        await configurationDbContext.ApiResources.AddAsync(new ApiResource
+        {
+            Name = Guid.NewGuid().ToString(),
+            DisplayName = "API",
+            Scopes = new List<string> {"https://www.example.com/api"}
+        }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.ApiScopes.AnyAsync())
+    {
+        await configurationDbContext.ApiScopes.AddAsync(new ApiScope
+        {
+            Name = "https://www.example.com/api",
+            DisplayName = "API"
+        }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.Clients.AnyAsync())
+    {
+        await configurationDbContext.Clients.AddRangeAsync(
+            new Client
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                ClientSecrets = new List<Secret> {new("secret".Sha512())},
+                ClientName = "Console Application",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = new List<string> {"https://www.example.com/api"},
+                AllowedCorsOrigins = new List<string> {"https://api:7001"}
+            }.ToEntity(),
+            new Client
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                ClientSecrets = new List<Secret> {new("secret".Sha512())},
+                ClientName = "Web Application",
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowedScopes = new List<string> {"openid", "profile", "email", "https://www.example.com/api"},
+                RedirectUris = new List<string> {"https://webapplication:7002/signin-oidc"},
+                PostLogoutRedirectUris = new List<string> {"https://webapplication:7002/signout-callback-oidc"}
+            }.ToEntity(),
+            new Client
+            {
+                ClientId = Guid.NewGuid().ToString(),
+                RequireClientSecret = false,
+                ClientName = "Single Page Application",
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowedScopes = new List<string> {"openid", "profile", "email", "https://www.example.com/api"},
+                AllowedCorsOrigins = new List<string> {"http://singlepageapplication:7003"},
+                RedirectUris =
+                    new List<string> {"http://singlepageapplication:7003/authentication/login-callback"},
+                PostLogoutRedirectUris =
+                    new List<string> {"http://singlepageapplication:7003/authentication/logout-callback"}
+            }.ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+
+    if (!await configurationDbContext.IdentityResources.AnyAsync())
+    {
+        await configurationDbContext.IdentityResources.AddRangeAsync(
+            new IdentityResources.OpenId().ToEntity(),
+            new IdentityResources.Profile().ToEntity(),
+            new IdentityResources.Email().ToEntity());
+
+        await configurationDbContext.SaveChangesAsync();
+    }
+}
 
 app.Run();
 
