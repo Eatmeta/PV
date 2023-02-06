50c8483e6755b2b77dde722cd9579cc9db500e8e
Eatmeta <metagroul@gmail.com>
2023-02-06 15:34:21 +0300

    Add DbContext and ApplicationUser

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,6 +1,30 @@
+using System.Reflection;
+using IdentityServer.Data;
+using IdentityServer.Factories;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, dbContextOptionsBuilder) =>
+{
+    dbContextOptionsBuilder.UseNpgsql(
+        serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString("Identity"),
+        NpgsqlOptionsAction);
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddClaimsPrincipalFactory<ApplicationUserClaimsPrincipalFactory>()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
+
+void NpgsqlOptionsAction(NpgsqlDbContextOptionsBuilder npgsqlDbContextOptionsBuilder)
+{
+    npgsqlDbContextOptionsBuilder.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+}
