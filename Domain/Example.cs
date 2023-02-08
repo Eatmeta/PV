15aad871cc7a87ca84bb4d24ff16047cae692675
Eatmeta <metagroul@gmail.com>
2023-02-08 17:37:07 +0300

    Add projects and implement Domain project

---
+++ Domain/Example.cs
@@ -0,0 +1,13 @@
+﻿namespace PhrasalVerb.Domain;
+
+public class Example
+{
+    public long ExampleId { get; set; }
+    public string ExampleFull { get; set; } = string.Empty;
+    public string ExampleFullUnderscore { get; set; } = string.Empty;
+    public string ExampleVerb { get; set; } = string.Empty;
+    public string ExampleParticle { get; set; } = string.Empty;
+    public string Meaning { get; set; } = string.Empty;
+    public string Verb { get; set; } = string.Empty;
+    public string VerbAndParticle { get; set; } = string.Empty;
+}
