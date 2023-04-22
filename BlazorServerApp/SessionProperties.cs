af81199cfacad3472f55da5221045b6b55cf441d
Eatmeta <metagroul@gmail.com>
2023-04-22 16:41:56 +0300

    Add User page and session properties storage

---
+++ BlazorServerApp/SessionProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerApp;
+
+public class SessionProperties
+{
+    public bool IsShowVerb { get; set; }
+    public bool IsShowParticles { get; set; }
+    
+    public SessionProperties()
+    {
+    }
+}
